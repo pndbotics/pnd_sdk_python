4382d8950560f5e50f4fc194ae828a40387e3de0
MOT <kid.mot.fan@gmail.com>
2025-11-29 18:24:09 +0800

    on realbot pass

--- pndbotics_sdk_py/idl/default.py
+++ pndbotics_sdk_py/idl/default.py
@@ -6,6 +6,7 @@ from .adam_u.msg.dds_ import MotorState_ as ADAMMotorState_
 from .adam_u.msg.dds_ import HandCmd_ as ADAMHandCmd_
 from .adam_u.msg.dds_ import HandState_ as ADAMHandState_
 
+from .pnd_adam.msg.dds_ import *
 
 """
 " std_msgs.msg.dds_ dafault
@@ -29,3 +30,51 @@ def adam_u_msg_dds__HandCmd_():
 
 def adam_u_msg_dds__HandState_():
     return ADAMHandState_([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0)
+
+
+# IDL for pnd_adam
+
+"""
+" std_msgs.msg.dds_ dafault
+"""
+def pnd_adam_msg_dds__WirelessController_():
+    return WirelessController_(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0)
+
+def pnd_adam_msg_dds__IMUState_():
+    return  IMUState_([0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], 0.0)
+
+def pnd_adam_msg_dds__MotorCmd_():
+    return MotorCmd_(0, 0.0, 0.0, 0.0, 0.0, 0.0, 0)
+
+def pnd_adam_msg_dds__MotorState_():
+    return MotorState_(0, 0.0, 0.0, 0.0, 0.0, 0, 0)
+
+
+# def pnd_adam_msg_dds__LowCmd_():
+#     return LowCmd_([pnd_adam_msg_dds__MotorCmd_() for i in range(35)], 0)
+
+def pnd_adam_msg_dds__LowCmd_(num):
+    # 使用空的 sequence 初始化 motor_cmd
+    return LowCmd_([pnd_adam_msg_dds__MotorCmd_() for i in range(num)], 0)
+
+# def pnd_adam_msg_dds__LowState_():
+#     return LowState_(0, pnd_adam_msg_dds__IMUState_(),
+#                 [pnd_adam_msg_dds__MotorState_() for i in range(35)],
+#                 0)
+
+def pnd_adam_msg_dds__LowState_(num):
+    # 使用空的 sequence 初始化 motor_cmd
+    return LowState_(pnd_adam_msg_dds__IMUState_(), [pnd_adam_msg_dds__MotorState_() for i in range(num)], 0)
+    # return LowState_([pnd_adam_msg_dds__MotorState_() for i in range(num)], 0)
+
+# def pnd_adam_msg_dds__LowState_():
+#     return LowState_(0, pnd_adam_msg_dds__IMUState_(),
+#                 [pnd_adam_msg_dds__MotorState_() for i in range(35)],
+#                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+#                 0)
+
+def pnd_adam_msg_dds__HandCmd_():
+    return HandCmd_([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0)
+
+def pnd_adam_msg_dds__HandState_():
+    return HandState_([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0)
